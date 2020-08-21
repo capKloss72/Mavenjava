b3c80bb2bd1247cd2be469ace6498ad99c9ab505
Tym Zon <tym.zon@gmail.com>
2020-08-21 11:18:28 +0800

    Initial commit

---
+++ src/test/java/com/capkloss/AppTest.java
@@ -0,0 +1,20 @@
+package com.capkloss;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/com/capkloss/SeleniumTest.java
@@ -0,0 +1,17 @@
+package com.capkloss;
+
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+    @Test
+    public void BrowserAutomation() {
+        System.out.println("BrowserAutomation");
+    }
+
+    @Test
+    public void elementsUI() {
+        System.out.println("elementsUI");
+    }
+}
